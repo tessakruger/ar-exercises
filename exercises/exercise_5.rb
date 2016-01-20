5715c39348839c5643ba7c92caae8799db0ff6a8
tessakruger <tessarfoto@gmail.com>
2016-01-20 14:52:51 -0500

    Completion of exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -11,3 +11,5 @@ all_stores_revenue = Store.sum(:annual_revenue)
 puts "Total revenue of all stores: $#{'%.02f' % all_stores_revenue}"
 average_revenue = Store.average(:annual_revenue)
 puts "Average total revenue of all stores: $#{'%.02f' % average_revenue}"
+highest_revenue_stores = Store.where("annual_revenue>(?)", 1000000).count
+puts "Number of stores with an annual revenue greater than $1,000,000: #{highest_revenue_stores}"
