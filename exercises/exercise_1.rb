8e3a0da8e75613dec0af9d3cb8e35b681053e7ea
tessakruger <tessarfoto@gmail.com>
2016-01-20 13:48:49 -0500

    Completion of exercises 1-3

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,23 +4,23 @@ puts "Exercise 1"
 puts "----------"
 
 # Create stores
-b = Store.create(name: "Burnaby",
+burnaby = Store.create(name: "Burnaby",
 annual_revenue: 300000,
 mens_apparel: true,
 womens_apparel: true
 )
 
-r = Store.create(name: "Richmond",
+richmond = Store.create(name: "Richmond",
 annual_revenue: 1260000,
 mens_apparel: false,
 womens_apparel: true
 )
 
-g = Store.create(name: "Gastown",
+gastown = Store.create(name: "Gastown",
 annual_revenue: 190000,
 mens_apparel: true,
 womens_apparel: false
 )
 
-puts "----------"
 puts "Total number of stores: #{Store.count}"
+puts "----------"
